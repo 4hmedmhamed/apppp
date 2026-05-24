906f3aba568f74d2a775eb2e89af3fb052c872f4
4hmedmhamed <ah5733956@gmail.com>
2026-05-24 21:37:46 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,88 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+const categories = [
+  { 
+    id: 1, 
+    name: 'غرفة المعيشة', 
+    img: 'https://images.unsplash.com/photo-1583847268964-b28dc8f51f92?auto=format&fit=crop&w=200&h=200&q=80' 
+  },
+  { 
+    id: 2, 
+    name: 'غرفة النوم', 
+    img: 'https://images.unsplash.com/photo-1598928506311-c55ded91a20c?auto=format&fit=crop&w=200&h=200&q=80' 
+  },
+  { 
+    id: 3, 
+    name: 'تناول الطعام', 
+    img: 'https://images.unsplash.com/photo-1577140917170-285929fb55b7?auto=format&fit=crop&w=200&h=200&q=80' 
+  },
+  { 
+    id: 4, 
+    name: 'المكتب المنزلي', 
+    img: 'https://images.unsplash.com/photo-1518455027359-f3f8164ba6bd?auto=format&fit=crop&w=200&h=200&q=80' 
+  },
+  { 
+    id: 5, 
+    name: 'أثاث خارجي', 
+    img: 'https://images.unsplash.com/photo-1533090161767-e6ffed986c88?auto=format&fit=crop&w=200&h=200&q=80' 
+  },
+  { 
+    id: 6, 
+    name: 'الإضاءة', 
+    img: 'https://images.unsplash.com/photo-1513506003901-1e6a229e2d15?auto=format&fit=crop&w=200&h=200&q=80' 
+  },
+  { 
+    id: 7, 
+    name: 'لمسات ديكورية', 
+    img: 'https://images.unsplash.com/photo-1616486338812-3dadae4b4ace?auto=format&fit=crop&w=200&h=200&q=80' 
+  },
+  { 
+    id: 8, 
+    name: 'المنسوجات', 
+    img: 'https://images.unsplash.com/photo-1545048702-79362596cdc9?auto=format&fit=crop&w=200&h=200&q=80' 
+  },
+  { 
+    id: 9, 
+    name: 'الحمام', 
+    img: 'https://images.unsplash.com/photo-1604014237800-1c9102c219da?auto=format&fit=crop&w=200&h=200&q=80' 
+  },
+  { 
+    id: 10, 
+    name: 'المطبخ', 
+    img: 'https://images.unsplash.com/photo-1556911220-bff31c812dba?auto=format&fit=crop&w=200&h=200&q=80' 
+  },
+
+  
+  { 
+    id: 13, 
+    name: 'ديكور الجدران', 
+    img: 'https://images.unsplash.com/photo-1513519245088-0e12902e5a38?auto=format&fit=crop&w=200&h=200&q=80' 
+  },
+  { 
+    id: 14, 
+    name: 'نباتات منزلية', 
+    img: 'https://images.unsplash.com/photo-1459156212016-c812468e2115?auto=format&fit=crop&w=200&h=200&q=80' 
+  },
+  { 
+    id: 15, 
+    name: 'تخزين وتنظيم', 
+    img: 'https://images.unsplash.com/photo-1595428774223-ef52624120d2?auto=format&fit=crop&w=200&h=200&q=80' 
+  }
+];
+  return (
+<>
+<div className='bg-success'>
+impootstrap/dist/css/bootstrap.mi
+
+</div>
+</>
+  )
+}
+
+export default App
